6d3734b75a4e98b8f7346d3f61961e2e071143ec
ThiagoNascimentoItu <thiago.nascimento57@etec.sp.gov.br>
2019-10-14 23:47:36 -0300

    Curso finalizado com sucesso

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
+const path = require('path');
+
 module.exports = {
-    entry:['@babel/polyfill','./src/main.js'],
-    output:{
-        path:__dirname + '/public',
-        filename:'bundle.js',
+    entry: ['@babel/polyfill', path.join(__dirname, 'src', 'main.js')],
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
     },
-    devServer:{
-            contentBase: __dirname + '/public',
-    },    
-    module:{
-        rules:[
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use:{
-                    loader: 'babel-loader',
-                }
-            }
-        ],
+    devServer: {
+      contentBase: path.join(__dirname, 'public')
     },
-};
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          }
+        }
+      ],
+    },
+  };
