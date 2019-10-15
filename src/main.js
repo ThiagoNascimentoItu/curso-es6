713429f0f995c9c7553ab2b22b7b81de761b162e
ThiagoNascimentoItu <thiago.nascimento57@etec.sp.gov.br>
2019-10-14 21:03:51 -0300

    Depencias instaladas e compilando

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-alert('teste');
+import axios from 'axios';
+
+class Api {
+    static async getUserInfo(usuername){
+        try{
+            const response = await axios.get(`https://api.github.com/user/${username}`);
+            console.log(response);
+        }catch(err){
+            console.log('Erro na API');
+        }
+    }
+}
+
+Api.getUserInfo('Thiago');
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,17 @@
 module.exports = {
-    entry:'./src/main.js',
+    entry:['@babel/polyfill','./src/main.js'],
     output:{
         path:__dirname + '/public',
         filename:'bundle.js',
     },
-    {
-        devServer:{
-            contentBase: __dirname + '/public'
-        }
-    },
+    devServer:{
+            contentBase: __dirname + '/public',
+    },    
     module:{
         rules:[
             {
-                test:/\.js$/,
-                exclude:/node_modules/,
+                test: /\.js$/,
+                exclude: /node_modules/,
                 use:{
                     loader: 'babel-loader',
                 }
